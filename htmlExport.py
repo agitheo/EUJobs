d2e85e576a7daf4fb27c2ec01a3d64fb803841b5
Agis Theodorakeas <agitheo@gmail.com>
2017-03-26 13:26:58 +0200

    - corrected the lineStats2.html link. Github is case sensitive

--- htmlExport.py
+++ htmlExport.py
@@ -40,7 +40,7 @@ background:#e9e9e9; \
 <body> \
 <div data-role="page" id="pageone"> \
   <div data-role="header"> \
-    <h1><a href="linestats2.html" target="_blank">'+str(len(current_possitions))+' Posts available from '+ str(intstNum)[1:-2] +' EU institutions</a></h1> \
+    <h1><a href="lineStats2.html" target="_blank">'+str(len(current_possitions))+' Posts available from '+ str(intstNum)[1:-2] +' EU institutions</a></h1> \
   </div> \
   <div data-role="main" class="ui-content"> \
     <form> \
